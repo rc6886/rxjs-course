b6be2b2ebc89d39c2387627719a784b8ed5b5bc2
Rodney Coleman Jr <rc6886@gmail.com>
2021-07-24 19:37:40 -0500

    section-2: Added the shareReplay operator.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,24 +11,27 @@ import { Course } from '../model/course';
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-    beginnerCourses: Course[];
-    advancedCourses: Course[];
+    beginnerCourses$: Observable<Course[]>;
+    advancedCourses$: Observable<Course[]>;
 
     constructor() {
     }
 
     ngOnInit() {
         const http$ = createHttpObservable('http://localhost:4201/api/courses');
+
         const courses$ = http$.pipe(
-            map(res => Object.values(res["payload"]) as Course[])
+            tap(() => console.log("HTTP request executed")),
+            map(res => Object.values(res["payload"]) as Course[]),
+            shareReplay()
         );
 
-        courses$.subscribe(
-            courses => {
-                this.beginnerCourses = courses.filter(course => course.category === 'BEGINNER');
-                this.advancedCourses = courses.filter(course => course.category === 'ADVANCED');
-            },
-            noop,
-            () => console.log('completed'));
+        this.beginnerCourses$ = courses$.pipe(
+            map(courses => courses.filter(course => course.category === 'BEGINNER'))
+        );
+
+        this.advancedCourses$ = courses$.pipe(
+            map(courses => courses.filter(course => course.category === 'ADVANCED'))
+        );
     }
 }
